9329a4ab9b1443c22a60130b2285c98747639bd5
ellofae <sergei.bykovskiy2003@gmail.com>
2023-03-26 22:26:43 +0300

    non-matching lines output and matching lines counter added

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -1,6 +1,8 @@
 #include "grep.h"
 #include "../cmd_reader/cmd_argv_reader.h"
 
+int caseIdentifier = 0;
+
 // Processing buffer implementation
 int grepIdentifier()
 {
@@ -24,7 +26,7 @@ int grepIdentifier()
             if (strcmp(*(cmdPtr + i), grepOptions[opt]) == 0)
             {
                 char option = *++grepOptions[opt];
-                identifier = 1;
+                identifier += 1;
 
                 switch (option)
                 {
@@ -55,15 +57,18 @@ int grepIdentifier()
 
         if (grepStructure.iFlag == 1)
         {
-            ignoreCase(cmdBuffer[nlines-1], cmdBuffer[nlines-2]);   
+            if (identifier == 1) {
+                ignoreCase(cmdBuffer[nlines-1], cmdBuffer[nlines-2]);
+            }
+            caseIdentifier = 1;
         }
         if (grepStructure.cFlag == 1)
         {
-        
+            matchingLinesCounter(cmdBuffer[nlines-1], cmdBuffer[nlines-2]);
         }
         if (grepStructure.vFlag == 1)
         {
-            
+            nonMatchingLinesOutput(cmdBuffer[nlines-1], cmdBuffer[nlines-2]);
         }
         if (grepStructure.lFlag == 1)
         {
@@ -138,6 +143,76 @@ int ignoreCase(char *filename, char *pattern) {
         }
     }
 
+    return 0;
+}
+
+ // -c flag function implementation
+int matchingLinesCounter(char *filename, char *pattern) {
+    FILE *fp;
+    if ((fp = fopen(filename, "r")) == NULL) {
+        printf("grep: didn't manage to open the file '%s'\n", filename);
+        return -1;
+    }
+
+    register int c;
+    char line[1024];
+    char counter = 0;
+
+    int newLineIdent = 0, len = 0;
+    while((c = getc(fp)) != EOF) {
+        *(line + len++) = c;
+
+        if(c == '\n') {
+            line[len] = '\0';
+            
+            len = 0;
+            if (caseIdentifier == 0) {
+                if(patternCheck(line, pattern) == 0) {
+                    counter++;
+                }
+            } else {
+                if(patternCheckIgnoreCase(line, pattern) == 0) {
+                    counter++;
+                }
+            }
+        }
+    }
+    printf("%d\n", counter);
+    
+    return 0;
+}
+
+// -v flag function implementation
+int nonMatchingLinesOutput(char *filename, char *pattern) {
+    FILE *fp;
+    if ((fp = fopen(filename, "r")) == NULL) {
+        printf("grep: didn't manage to open the file '%s'\n", filename);
+        return -1;
+    }
+
+    register int c;
+    char line[1024];
+
+    int newLineIdent = 0, len = 0;
+    while((c = getc(fp)) != EOF) {
+        *(line + len++) = c;
+
+        if(c == '\n') {
+            line[len] = '\0';
+            
+            len = 0;
+            if (caseIdentifier == 0) {
+                if(patternCheck(line, pattern) != 0) {
+                    printf("%s", line);
+                }
+            } else {
+                if(patternCheckIgnoreCase(line, pattern) != 0) {
+                    printf("%s", line);
+                }
+            }
+        }
+    }
+    
     return 0;
 }
 
--- src/grep/grep.h
+++ src/grep/grep.h
@@ -8,13 +8,15 @@
 struct grepStruct {
     int iFlag; // --ignore-case
     int cFlag; // count of matching lines
-    int vFlag; // count non-matching lines
+    int vFlag; // print non-matching lines
     int lFlag; // file names that have matches
     int LFlag; // file names that don't have matches
 };
 
 int grepIdentifier();
 int ignoreCase(char *filename, char *pattern); // -i flag function
+int matchingLinesCounter(char *filename, char *pattern); // -c flag function
+int nonMatchingLinesOutput(char *filename, char *pattern); // -v flag function
 
 int regularGrep(char *filename, char *pattern); // regular grep function
 int patternCheck(char *line, char* pattern); // check if pattern is in the line
